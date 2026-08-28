deaf10639415576ed18a3f9ce3b4b23ba1e87816
Lena_Bokhan <leliklena@i.ua>
2026-08-28 23:09:23 +0300

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,21 @@
-export const Person = ({ name, age, sex, isMarried, partnerName }) => {
-  const partner = sex === 'f' ? 'husband' : 'wife';
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => {
+  // prettier-ignore
+  const partner = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   return (
+    // prettier-ignore
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">
-        {isMarried ? `${partnerName} is my ${partner}` : 'I am not married'}
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
       </p>
     </section>
   );
